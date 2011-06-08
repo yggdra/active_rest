cb3f57d272b53fd2fbfde606e2dfabe75bfdac49
Daniele Orlandi <daniele@orlandi.com>
2011-06-08 23:16:22 +0200

    Removed embedded-models and integrated new options in init.rb

--- init.rb
+++ init.rb
@@ -11,3 +11,11 @@
 #
 
 require 'active_rest/routes'
+
+require 'active_record'
+
+module ActiveRecord::Associations::ClassMethods
+  @@valid_keys_for_has_many_association << :embedded
+  @@valid_keys_for_has_one_association << :embedded << :embedded_in
+  @@valid_keys_for_belongs_to_association << :embedded << :embedded_in
+end
