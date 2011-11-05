c439c2c74dd93a76a123c4b63abdd373ecedbf5c
Daniele Orlandi <daniele@orlandi.com>
2011-11-05 03:44:30 +0100

    First support for Rails 3.1

--- lib/active_rest.rb
+++ lib/active_rest.rb
@@ -7,9 +7,24 @@ require 'active_rest/controller'
 
 require 'active_record'
 
-module ActiveRecord::Associations::ClassMethods
-  @@valid_keys_for_has_many_association << :embedded
-  @@valid_keys_for_has_one_association << :embedded << :embedded_in
-  @@valid_keys_for_belongs_to_association << :embedded << :embedded_in
-end
+# Rails 3.0
+#module ActiveRecord::Associations::ClassMethods
+#  @@valid_keys_for_has_many_association << :embedded
+#  @@valid_keys_for_has_one_association << :embedded << :embedded_in
+#  @@valid_keys_for_belongs_to_association << :embedded << :embedded_in
+#end
 
+# Rails 3.1
+module ActiveRecord::Associations::Builder
+  class HasMany
+    self.valid_options += [ :embedded ]
+  end
+
+  class HasOne
+    self.valid_options += [ :embedded, :embedded_in ]
+  end
+
+  class BelongsTo
+    self.valid_options += [ :embedded, :embedded_in ]
+  end
+end
